dbc401fcf4172c47df9fd87bb9df12cd31c50a85
knightCh <amankumarpandey2397@gmail.com>
2021-08-17 22:30:01 +0530

    complete portfolio template

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    $(window).on('load',function(){
+        $('.preloader').addClass('complete');
+    });
+
+    $(window).on('scroll',function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>=50){
+            $(".sticky").addClass("stickyadd");
+        }else{
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+
+    var typed = new Typed(".element",{
+        strings: ["Raju","a Developer","a Designer","a Businessman"],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1500
+    });
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp__id'),
+        handler: function() {
+            var p = document.querySelectorAll('.progress-bar');
+            p[0].setAttribute("style","width:100%;transition:1s all");
+            p[1].setAttribute("style","width:95%;transition:1.5s all");
+            p[2].setAttribute("style","width:70%;transition:1.7s all");
+            p[3].setAttribute("style","width:75%;transition:2s all");
+            p[4].setAttribute("style","width:70%;transition:2.5s all");
+            p[5].setAttribute("style","width:92%;transition:2.7s all");
+        },
+        offset:'90%'
+    });
+
+    var filterizd = $('.filter__container').filterizr({
+        animationDuration:0.5,
+    });
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        items:1
+    });
+
+    
+
+});
+
